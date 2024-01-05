c86b6c7bd711fb4af3705282d23355b2fc5f10ed
Soja-b <149033709+Soja-b@users.noreply.github.com>
2024-01-05 09:52:04 +0530

    Add files via upload

---
+++ alpha.py
@@ -0,0 +1,16 @@
+n=int(input("enter the number of names in the list:"))
+names=[]
+for i in range(0,n):
+	items=input("enter the names:")
+	names.append(items)
+names.sort()
+for x in names:
+	print("in alphabetic order:")
+	print(x)                                                 
+                                                         
+                                                         
+                                                         
+                                                         
+                                                         
+                                                         
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
